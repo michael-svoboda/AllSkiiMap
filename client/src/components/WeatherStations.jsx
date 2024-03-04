382240a1c22228f89ee1f06ba9bb106936afb464
T-o-mas <tomaskmet23@gmail.com>
2024-03-04 08:27:54 -0700

    added zooming

--- client/src/components/WeatherStations.jsx
+++ client/src/components/WeatherStations.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import fernieImage from "../images/fernie.png"
+import kickingHorseImage from "../images/kicking-horse.png"
+import nakiskaImage from "../images/nakiska.png"
+import sunshineImage from "../images/sunshine.png"
+import lakeLouiseImage from "../images/lakeLouise.png"
+import revelstokeImage from "../images/revelstoke.png"
+import panoramaImage from "../images/panorama.png"
+import norquayImage from "../images/norquay.png"
+import kimberleyImage from "../images/kimberley.png"
+import silverStarImage from "../images/silverStar.png"
+import sunPeaksImage from "../images/sunPeaks.png"
+import bigWhiteImage from "../images/bigWhite.png"
 
 const MapboxSkiRuns = () => {
   useEffect(() => {
@@ -12,6 +23,7 @@ const MapboxSkiRuns = () => {
       style: "mapbox://styles/mapbox/satellite-streets-v12",
       center: [-115, 51], // Adjust the center to focus on Alberta or your desired location
       zoom: 6,
+      pitch: 45
     });
 
     const zoom = (index, angle) => { 
@@ -22,6 +34,53 @@ const MapboxSkiRuns = () => {
         bearing: angle,
       });
     }
+
+    function loadImageAndAddToMap(map, imageUrl, imageName, coordinates, layerId, bearing, source, iconSize) {
+      map.loadImage(imageUrl, (error, image) => {
+        if (error) throw error;
+    
+        // Add the image to the map style.
+        map.addImage(imageName, image);
+    
+        // Add a data source containing one point feature.
+        map.addSource(source, {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: coordinates,
+                },
+              },
+            ],
+          },
+        });
+    
+        // Add a layer to use the image to represent the data.
+        map.addLayer({
+          id: layerId,
+          type: "symbol",
+          source: source, // reference the data source
+          layout: {
+            "icon-image": imageName, // reference the image
+            "icon-size": iconSize,
+          },
+        });
+    
+        // Add click event listener for the 'points' layer
+        map.on("click", layerId, (e) => {
+          // Display the message or handle the click event as needed
+          //zoom
+          zoom(coordinates, bearing);
+        });
+      });
+    }
+
+
+
 
     map.on("load", () => {
       // Replace the 'data.geojson' with the path to your generated GeoJSON file
@@ -69,56 +128,29 @@ const MapboxSkiRuns = () => {
         tileSize: 512,
         maxzoom: 14,
       });
+      
+
+      
 
       // add the DEM source as a terrain layer with exaggerated height
       map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+      
+      // Usage
+      loadImageAndAddToMap(map, fernieImage, "fernie", [-115.0873, 49.4627], "Ski-marker1", -100, "point1", 0.05);
+      loadImageAndAddToMap(map, kickingHorseImage, "kickinghorse", [-117.0483, 51.2976], "Ski-marker2", -100, "point2", 0.10);
+      loadImageAndAddToMap(map, nakiskaImage, "nakiska", [-115.1511, 50.9427], "Ski-marker3", -40, "point3", 0.15);
+      loadImageAndAddToMap(map, sunshineImage, "sunshine", [-115.7765, 51.0785], "Ski-marker4", 130, "point4", 0.10);
+      loadImageAndAddToMap(map, lakeLouiseImage, "lakelouise", [-116.1622, 51.4419], "Ski-marker5", 40, "point5", 0.077);
+      loadImageAndAddToMap(map, revelstokeImage, "revelstoke", [-118.1631, 50.9584], "Ski-marker6", 90, "point6", 0.10);
+      loadImageAndAddToMap(map, panoramaImage, "panorama", [-116.238157, 50.460374], "Ski-marker7", 150, "point7", 0.077);
+      loadImageAndAddToMap(map, norquayImage, "norquay", [-115.6068, 51.2053], "Ski-marker8", 220, "point8", 0.05);
+      loadImageAndAddToMap(map, kimberleyImage, "kimberley", [-116.0048, 49.6879], "Ski-marker9", 220, "point9", 0.05);
+      loadImageAndAddToMap(map, silverStarImage, "silverStar", [-119.0610, 50.3598], "Ski-marker10", 60, "point10", 0.08);
+      loadImageAndAddToMap(map, sunPeaksImage, "sunPeaks", [-119.8891, 50.8837], "Ski-marker11", 60, "point11", 0.08);
+      loadImageAndAddToMap(map, bigWhiteImage, "bigWhite", [-118.93528, 49.7160], "Ski-marker12", 30, "point12", 0.08);
 
-      map.loadImage(
-        fernieImage,
-        (error, image) => {
-          if (error) throw error;
 
-          // Add the image to the map style.
-          map.addImage("fernie", image);
 
-          // Add a data source containing one point feature.
-          map.addSource("point", {
-            type: "geojson",
-            data: {
-              type: "FeatureCollection",
-              features: [
-                {
-                  type: "Feature",
-                  geometry: {
-                    type: "Point",
-                    coordinates: [-115.0873, 49.4627],
-                  },
-                },
-              ],
-            },
-          });
-
-          // Add a layer to use the image to represent the data.
-          map.addLayer({
-            id: "Ski-markers",
-            type: "symbol",
-            source: "point", // reference the data source
-            layout: {
-              "icon-image": "fernie", // reference the image
-              "icon-size": 0.05,
-            },
-          });
-          // Add click event listener for the 'points' layer
-          map.on("click", "Ski-markers", (e) => {
-            // Display the message or handle the click event as needed
-            window.alert("Fernie Zoom");
-            console.log("Fernie Zoom")
-            //zoom
-            zoom([-115.0873, 49.4627], -100)
-
-          });
-        }
-      );
     });
 
     // Cleanup on component unmount
